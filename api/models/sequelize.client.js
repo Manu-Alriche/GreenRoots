805d228f8d9cafa3037dda261efdce5634e9b58a
Manu-Alriche <e.alriche@yahoo.com>
2025-09-12 16:18:19 +0200

    Ajout de la gestion des options de dialecte pour la connexion à PostgreSQL et amélioration de la vérification de la connexion avec des messages d'erreur.

--- api/models/sequelize.client.js
+++ api/models/sequelize.client.js
@@ -2,11 +2,24 @@ import "dotenv/config";
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(process.env.PG_URL, {
+  dialect: "postgres",
+  logging: false,
   define: {
     createdAt: "created_at",
-    updatedAt: "updated_at"
+    updatedAt: "updated_at",
   },
-  logging: false
-}); 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // important pour Railway
+    },
+  },
+});
 
-await sequelize.authenticate();
+// Vérifie la connexion
+try {
+  await sequelize.authenticate();
+  console.log("✅ Connecté à PostgreSQL");
+} catch (err) {
+  console.error("❌ Erreur connexion DB :", err);
+}
